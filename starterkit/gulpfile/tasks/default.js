2c9bf2b8cc3b939258b223feca1bdf3d68b33a19
Fabrizio Meinero <imfaberdev@gmail.com>
2018-02-19 16:46:54 +0000

    Fix default task when there aren't pre build tasks

--- starterkit/gulpfile/tasks/default.js
+++ starterkit/gulpfile/tasks/default.js
@@ -10,26 +10,30 @@ const defaultTask = function () {
   const preBuildTasks = [],
         defaultTasks  = [];
 
-  // First check pre build tasks such as vendor.
-  if (taskHelper.isTaskEnabled('vendor')) {
-    preBuildTasks.push('vendor');
-  }
-
-  // Then the default tasks
+  // Make an array of default tasks.
   ['browserSync', 'image', 'font', 'sass', 'javascript'].forEach((task) => {
     if (taskHelper.isTaskEnabled(task)) {
       defaultTasks.push(task);
     }
   });
 
-  gulpSequence('clean', preBuildTasks)((err) => {
-    if (err) console.log(err);
-    else {
-      gulpSequence(defaultTasks, 'watch')((err) => {
-        if (err) console.log(err)
-      });
-    }
-  });
+  // If needs some pre build tasks such as vendor run them right after clean.
+  if (taskHelper.isTaskEnabled('vendor')) {
+    preBuildTasks.push('vendor');
+    gulpSequence('clean', preBuildTasks)((err) => {
+      if (err) console.log(err);
+      else {
+        gulpSequence(defaultTasks, 'watch')((err) => {
+          if (err) console.log(err)
+        });
+      }
+    });
+  }
+  else {
+    gulpSequence('clean', defaultTasks, 'watch')((err) => {
+      if (err) console.log(err)
+    });
+  }
 };
 
 gulp.task('default', defaultTask);
